f1888d0b3c2a5ccad15067a11201dde30c05341d
Andrea Zucchini <azga84@gmail.com>
2017-09-08 20:37:27 +0200

    create interfaces

---
+++ src/EpicBattle/Interfaces/Warrior.ts
@@ -0,0 +1,6 @@
+import { Weapon } from './Weapon';
+
+export interface Warrior {
+  name: string;
+  weapon: Weapon;
+}
---
+++ src/EpicBattle/Interfaces/index.ts
@@ -0,0 +1,3 @@
+export { Battle } from './Battle';
+export { Weapon } from './Weapon';
+export { Warrior } from './Warrior';
