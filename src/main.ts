8449070d911d6a18d86224e426fed0e4df47cf0e
Andrea Zucchini <azga84@gmail.com>
2017-09-08 22:50:00 +0200

    add order use case

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { IDENTIFIERS } from './EpicBattle/Constants/identifiers';
 import { Battle } from './EpicBattle/Interfaces';
 import container from './EpicBattle/Configuration/ioc';
+import { shoppingContainer } from './Shopping/Configuration/ioc';
+import { SERVICE_IDENTIFIERS } from './Shopping/Constants/identifiers';
+import { IOrder } from './Shopping/Interfaces';
 
 // Composition root
 const epicBattle = container.get<Battle>(IDENTIFIERS.BATTLE);
+const order = shoppingContainer.get<IOrder>(SERVICE_IDENTIFIERS.ORDER);
 
 console.log(epicBattle.fight());
+console.log(order.show());
